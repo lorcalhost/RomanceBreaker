6e75f1e5ed4477f054d769a6097d6e011a39c3f5
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2018-12-04 13:36:30 +0100

    Update WA-RomanceBreaker.py

--- WA-RomanceBreaker.py
+++ WA-RomanceBreaker.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,17 +8,18 @@ import time
 import sys
 import datetime
 import random
+import pyperclip
 
 #Add your own custom messages by putting your message between "s and using the same format below
-customMsgs = ["Good morning beautiful", "I'm too lazy", "To write messages on my own", "Custom message #4"]
+customMsgs = ["Good morning beautiful ♥♥♥", "I'm too lazy", "To write messages on my own", "Custom message #4"]
 #Put your own custom time interval here, make sure the hour is always two digits
-customTimeInterval = ["18:21", "18:25"]
+customTimeInterval = ["04:20", "16:20"]
 
 randTimeHour = 0
 randTimeMinute = 0
 
 #Getting username & pwd
-bae = nput("Name of your bae: ")
+bae = input("Name of your bae: ")
 bae = str('"' + bae + '"')
 print("A popup view of WhatsApp web will now open,\nScan the QR code in the page via your app\nDon't close the popup")
 time.sleep(5)
@@ -27,20 +29,21 @@ driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
 
 def morningMessage():
-	#Sending message
-	msg = random.choice(customMsgs)
-	global driver
-	global bae
+    #Sending message
+    global driver
+    global bae
+    msg = random.choice(customMsgs)
+    pyperclip.copy(msg) #Copies random message to clipboard
 
-	#Some Selenium stuff, don't bother
-	x_arg = '//span[contains(@title,' + bae + ')]'
-	group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
-	group_title.click()
-	message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
-	message.send_keys(msg) #Sends pre selected random message
-	sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
-	sendbutton.click() #Presses send button
-	return
+    #Some Selenium stuff, don't bother
+    x_arg = '//span[contains(@title,' + bae + ')]'
+    group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+    group_title.click()
+    message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+    message.send_keys(Keys.CONTROL, 'v') #Sends message from clipboard
+    sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+    sendbutton.click() #Presses send button
+    return
 
 def newRandTime():
     global customTimeInterval
