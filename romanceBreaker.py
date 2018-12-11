dcf7da1868aa6e7a49b07616ad8a58710dd76642
lorcalhost <callegari.lorenzo@gmail.com>
2018-12-11 17:24:41 +0100

    Fixed an argument out of range error

--- romanceBreaker.py
+++ romanceBreaker.py
@@ -6,19 +6,19 @@ def prnthelp():
     print("Welcome to the RomanceBreaker user guide\nPlease refer to the Github page for detailed setup instructions")
     print("\nList of commands:\nwhatsapp or -w ---> launches RomanceBreaker in WhatsApp mode\nmessenger or -m ---> launches RomanceBreaker in Messenger mode\nsms or -s ---> launches RomanceBreaker in SMS mode")
     print("help or -h or man ---> Launches user guide\nupdate ---> Updates RomanceBreaker from the github repo")
-
-if str(sys.argv[1]) == "whatsapp" or str(sys.argv[1]) == "-w":
-    print("WHATSAPP MODE")
-    os.system("python whatsappRB.py")
-elif str(sys.argv[1]) == "messenger" or str(sys.argv[1]) == "-m":
-    print("MESSENGER MODE")
-    os.system("python messengerRB.py")
-elif str(sys.argv[1]) == "sms" or str(sys.argv[1]) == "-s":
-    print("SMS MODE")
-    os.system("python smsRB.py")
-elif str(sys.argv[1]) == "help" or str(sys.argv[1]) == "-h" or str(sys.argv[1]) == "man":
-    prnthelp()
-elif str(sys.argv[1]) == "update":
-    os.system("git pull")
-else:
+try:
+    if str(sys.argv[1]) == "whatsapp" or str(sys.argv[1]) == "-w":
+        print("WHATSAPP MODE")
+        os.system("python whatsappRB.py")
+    elif str(sys.argv[1]) == "messenger" or str(sys.argv[1]) == "-m":
+        print("MESSENGER MODE")
+        os.system("python messengerRB.py")
+    elif str(sys.argv[1]) == "sms" or str(sys.argv[1]) == "-s":
+        print("SMS MODE")
+        os.system("python smsRB.py")
+    elif str(sys.argv[1]) == "help" or str(sys.argv[1]) == "-h" or str(sys.argv[1]) == "man":
+        prnthelp()
+    elif str(sys.argv[1]) == "update":
+        os.system("git pull")
+except:
     print("Invalid arguments\nRun python romanceBreaker.py -h to look at the commands manual")
